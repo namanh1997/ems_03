4c7938e04c0e6292c889b84e9371731bf25dc482
namanh1997 <nguyennamnh2197@gmail.com>
2019-08-27 10:06:33 +0700

    show_exams

--- app/models/trainee_exam.rb
+++ app/models/trainee_exam.rb
@@ -1,4 +1,15 @@
 class TraineeExam < ApplicationRecord
   belongs_to :exam
   belongs_to :user
+  has_many :detail_exams
+
+  scope :sort_by_subject_and_name,
+    ->{includes({exam: :subject}, :user).order(created_at: :desc)}
+
+  delegate :subject_name, :name, to: :exam, prefix: true
+  delegate :name, to: :user, prefix: true
+
+  validates_inclusion_of :is_passed,
+    in: [true, false, "true", "false", 1, 0]
+  validates :complete_time, presence: true
 end
