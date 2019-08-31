710fbddf774f9f745afa2d5d7f9d8c805000cef5
namanh1997 <nguyennamnh2197@gmail.com>
2019-08-31 18:58:49 +0700

    do exam final

--- app/models/detail_exam_answer.rb
+++ app/models/detail_exam_answer.rb
@@ -1,6 +1,4 @@
 class DetailExamAnswer < ApplicationRecord
   belongs_to :detail_exam
   belongs_to :answer
-
-  
 end
--- app/models/trainee_exam.rb
+++ app/models/trainee_exam.rb
@@ -1,19 +1,15 @@
 class TraineeExam < ApplicationRecord
-  TRAINEE_EXAM_PARAMS = [:complete_time,
+  TRAINEE_EXAM_PARAMS = [:complete_time, :exam_id,
     detail_exams_attributes: [:question_id,
-      detail_exam_answers_attributes:[:checked]]].freeze
+    detail_exam_answers_attributes: [:checked, :answer_id, :id]]].freeze
   belongs_to :exam
-  belongs_to :user  
-  has_many :detail_exams, dependent: :destroy 
+  belongs_to :user
+  has_many :detail_exams, dependent: :destroy
 
   scope :sort_by_subject_and_name, ->{order created_at: :desc}
 
-  validates_inclusion_of :is_passed,
-    in: [true, false, "true", "false", 1, 0]
   validates :complete_time, presence: true
 
   accepts_nested_attributes_for :detail_exams,
     allow_destroy: true
-
-  delegate :checked, to: :detail_exams
 end
